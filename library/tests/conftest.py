c34850a08590be00a1e838a8340e48bad2753639
argtus <11436442+argtus@users.noreply.github.com>
2024-05-18 15:24:26 +0300

    Update conftest.py

--- library/tests/conftest.py
+++ library/tests/conftest.py
@@ -3,6 +3,7 @@ These allow the mocking of various Python modules
 that might otherwise have runtime side-effects.
 """
 import sys
+
 import mock
 import pytest
 from i2cdevice import MockSMBus
@@ -20,106 +21,97 @@ class SMBusFakeDeviceNoTimeout(MockSMBus):
         self.regs[0x00:0x01] = 0x0f, 0x80
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def cleanup():
     yield None
-    try:
-        del sys.modules['enviroplus']
-    except KeyError:
-        pass
-    try:
-        del sys.modules['enviroplus.noise']
-    except KeyError:
-        pass
-    try:
-        del sys.modules['enviroplus.gas']
-    except KeyError:
-        pass
-
-
-@pytest.fixture(scope='function', autouse=False)
-def GPIO():
-    """Mock RPi.GPIO module."""
-    GPIO = mock.MagicMock()
-    # Fudge for Python < 37 (possibly earlier)
-    sys.modules['RPi'] = mock.Mock()
-    sys.modules['RPi'].GPIO = GPIO
-    sys.modules['RPi.GPIO'] = GPIO
-    yield GPIO
-    del sys.modules['RPi']
-    del sys.modules['RPi.GPIO']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def spidev():
-    """Mock spidev module."""
-    spidev = mock.MagicMock()
-    sys.modules['spidev'] = spidev
-    yield spidev
-    del sys.modules['spidev']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def smbus():
-    """Mock smbus module."""
-    smbus = mock.MagicMock()
-    smbus.SMBus = SMBusFakeDevice
-    sys.modules['smbus'] = smbus
-    yield smbus
-    del sys.modules['smbus']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def smbus_notimeout():
-    """Mock smbus module."""
-    smbus = mock.MagicMock()
-    smbus.SMBus = SMBusFakeDeviceNoTimeout
-    sys.modules['smbus'] = smbus
-    yield smbus
-    del sys.modules['smbus']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def mocksmbus():
-    """Mock smbus module."""
-    smbus = mock.MagicMock()
-    sys.modules['smbus'] = smbus
-    yield smbus
-    del sys.modules['smbus']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def atexit():
-    """Mock atexit module."""
-    atexit = mock.MagicMock()
-    sys.modules['atexit'] = atexit
-    yield atexit
-    del sys.modules['atexit']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def sounddevice():
-    """Mock sounddevice module."""
-    sounddevice = mock.MagicMock()
-    sys.modules['sounddevice'] = sounddevice
-    yield sounddevice
-    del sys.modules['sounddevice']
-
-
-@pytest.fixture(scope='function', autouse=False)
-def numpy():
-    """Mock numpy module."""
-    numpy = mock.MagicMock()
-    sys.modules['numpy'] = numpy
-    yield numpy
-    del sys.modules['numpy']
+    modules = "enviroplus", "enviroplus.noise", "enviroplus.gas", "ads1015", "i2cdevice"
+    for module in modules:
+        try:
+            del sys.modules[module]
+        except KeyError:
+            pass
 
 
 @pytest.fixture(scope="function", autouse=False)
-def gas():
-    gas = mock.MagicMock()
-    gas.available.return_value = True
-    gas.read_all.return_value = mock.Mock(oxidising=16641.0, reducing=16727.0, nh3=16813.0, adc=0.255)
-    sys.modules["enviroplus.gas"] = gas
-    yield gas
-    del sys.modules["enviroplus.gas"]
+def gpiod():
+    sys.modules["gpiod"] = mock.Mock()
+    sys.modules["gpiod.line"] = mock.Mock()
+    yield sys.modules["gpiod"]
+    del sys.modules["gpiod.line"]
+    del sys.modules["gpiod"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def gpiodevice():
+    gpiodevice = mock.Mock()
+    gpiodevice.get_pins_for_platform.return_value = [(mock.Mock(), 0)]
+    gpiodevice.get_pin.return_value = (mock.Mock(), 0)
+
+    sys.modules["gpiodevice"] = gpiodevice
+    yield gpiodevice
+    del sys.modules["gpiodevice"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def spidev():
+    """Mock spidev module."""
+    spidev = mock.MagicMock()
+    sys.modules["spidev"] = spidev
+    yield spidev
+    del sys.modules["spidev"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def smbus():
+    """Mock smbus2 module."""
+    smbus = mock.MagicMock()
+    smbus.SMBus = SMBusFakeDevice
+    sys.modules["smbus2"] = smbus
+    yield smbus
+    del sys.modules["smbus2"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def smbus_notimeout():
+    """Mock smbus2 module."""
+    smbus = mock.MagicMock()
+    smbus.SMBus = SMBusFakeDeviceNoTimeout
+    sys.modules["smbus2"] = smbus
+    yield smbus
+    del sys.modules["smbus2"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def mocksmbus():
+    """Mock smbus2 module."""
+    smbus = mock.MagicMock()
+    sys.modules["smbus2"] = smbus
+    yield smbus
+    del sys.modules["smbus2"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def atexit():
+    """Mock atexit module."""
+    atexit = mock.MagicMock()
+    sys.modules["atexit"] = atexit
+    yield atexit
+    del sys.modules["atexit"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def sounddevice():
+    """Mock sounddevice module."""
+    sounddevice = mock.MagicMock()
+    sys.modules["sounddevice"] = sounddevice
+    yield sounddevice
+    del sys.modules["sounddevice"]
+
+
+@pytest.fixture(scope="function", autouse=False)
+def numpy():
+    """Mock numpy module."""
+    numpy = mock.MagicMock()
+    sys.modules["numpy"] = numpy
+    yield numpy
+    del sys.modules["numpy"]
